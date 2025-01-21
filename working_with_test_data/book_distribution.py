bc604bf481c5c9778c6366c2959e9cc28e72f111
dmitry.kozaev <dmitry.kozaev@im.systems>
2025-01-21 17:37:17 +0300

    DZ4 работа по комментариям(часть 2)

--- working_with_test_data/book_distribution.py
+++ working_with_test_data/book_distribution.py
@@ -2,6 +2,12 @@ import csv
 import json
 
 
+def read_users_from_json(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        users = json.load(file)
+    return users
+
+
 def read_books_from_csv(file_path):
     books = []
     with open(file_path, 'r', encoding='utf-8') as file:
@@ -11,13 +17,11 @@ def read_books_from_csv(file_path):
     return books
 
 
-users = [{'name': 'User1', 'books': []},
-         {'name': 'User2', 'books': []},
-         {'name': 'User3', 'books': []}]
+file_path_users = 'users.json'
+file_path_books = 'books.csv'
 
-file_path = 'books.csv'
-
-books = read_books_from_csv(file_path)
+books = read_books_from_csv(file_path_books)
+users = read_users_from_json(file_path_users)
 
 
 def distribute_books(books, users):
@@ -28,18 +32,15 @@ def distribute_books(books, users):
     remainder = num_books % num_users
 
     book_index = 0
-    for user in users:
-        user['books'] = books[book_index:book_index + base_count]
-        book_index += base_count
-
-        if remainder > 0:
-            user['books'].append(books[book_index])
-            book_index += 1
-            remainder -= 1
+    for i, user in enumerate(users):
+        count_for_user = base_count + (1 if i < remainder else 0)
+        user['books'] = books[book_index:book_index + count_for_user]
+        book_index += count_for_user
 
     return users
 
 
+distribute_books(books, users)
 distributed_users = distribute_books(books, users)
 with open('result.json', 'w', encoding='utf-8') as result_file:
     json.dump(distributed_users, result_file, ensure_ascii=False, indent=4)
@@ -47,3 +48,6 @@ with open('result.json', 'w', encoding='utf-8') as result_file:
 json_result = json.dumps(distributed_users, ensure_ascii=False, indent=4)
 
 print(json_result)
+
+for user in users:
+    print(f"{user['name']} получит {len(user['books'])} книг.")
